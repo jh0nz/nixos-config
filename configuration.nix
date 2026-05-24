949e4b92406bb8477d7b4070b95869bb1d90704a
jh0nz <jhondeycraft776@gmail.com>
2026-05-24 16:05:54 -0400

    magno config

--- configuration.nix
+++ configuration.nix
@@ -97,7 +97,8 @@ virtualisation.docker.enable = true;
    zlib
  ];	
         programs.niri.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages;
+	programs.mangowc.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
  
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
